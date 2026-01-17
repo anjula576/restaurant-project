54c64de2de4641950f850fcfe88941a90c64703f
anjula576 <anjula.183.gcc@gmail.com>
2026-01-17 22:33:16 +0530

    after setup the main function :

--- backend/restaurant/src/main/java/com/example/restaurant/RestaurantApplication.java
+++ backend/restaurant/src/main/java/com/example/restaurant/RestaurantApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestaurantApplication {
 
+	// this is the main function of the appleication
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantApplication.class, args);
 
